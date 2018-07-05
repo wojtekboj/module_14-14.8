d789a5302375c282aafd36dc833e4dbdeb032665
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-05 17:49:48 +0200

    Modification App.js

--- components/App.js
+++ components/App.js
@@ -7,7 +7,7 @@ App = React.createClass({
         };
     },
     getGif: function (searchingText, callback) { // 1.
-        var url = 'http://api.giphy.com' + '/v1/gifs/random?api_key=' + 'Ahz6kKvHoNTrwWXaZYfD2P3uhpLF60NE' + '&tag=' + searchingText; // 2.
+        var url = 'https://api.giphy.com' + '/v1/gifs/random?api_key=' + 'Ahz6kKvHoNTrwWXaZYfD2P3uhpLF60NE' + '&tag=' + searchingText; // 2.
         var xhr = new XMLHttpRequest(); // 3.
         xhr.open('GET', url);
         xhr.onload = function () {
@@ -52,7 +52,7 @@ App = React.createClass({
             } >
             <
             h1 > Wyszukiwarka GIFow! < /h1> <
-            p > Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
+            p > Znajdź gifa na < a href = 'https://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
             <
             Search onSearch = {
                 this.handleSearch
