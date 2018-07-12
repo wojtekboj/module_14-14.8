372490b74e6ea15f84abf8925f23b7e23081a5c8
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-12 07:59:29 +0200

    Modification Gif.js

--- components/Gif.js
+++ components/Gif.js
@@ -11,30 +11,11 @@ Gif = React.createClass({
     render: function () {
         var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
 
-        return ( <
-            div style = {
-                styles
-            } >
-            <
-            a href = {
-                this.getUrl()
-            }
-            title = 'view this on giphy'
-            target = 'new' >
-            <
-            img id = 'gif'
-            src = {
-                url
-            }
-            style = {
-                {
-                    width: '100%',
-                    maxWidth: '350px'
-                }
-            }
-            /> <
-            /a> <
-            /div>
+        return ( 
+            <div style={styles}>
+            <a href = {this.getUrl()} title = 'view this on giphy' target = 'new' >
+                <img id = 'gif' src = {url} style = {{width: '100%', maxWidth: '350px'}}/></a> 
+            </div>
         );
     }
 });
